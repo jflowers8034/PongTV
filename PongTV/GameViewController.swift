2a52c00e6e2120c87a077f80b4d8ab86b09568ee
Albi <amuho1246@stu.d214.org>
2018-05-07 11:29:14 -0500

    added music

--- PongTV/GameViewController.swift
+++ PongTV/GameViewController.swift
@@ -48,14 +48,14 @@ class GameViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        }
+    
+
+    @IBAction func startButton(_ sender: Any) {
+        musicEffect.play()
     }
+    
+    
+    
+    
 }
