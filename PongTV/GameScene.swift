f68472a985462e1c3847b4700d51e854e404c142
JP <jflowers8034@stu.d214.org>
2018-05-09 11:41:04 -0500

    added scoring

--- PongTV/GameScene.swift
+++ PongTV/GameScene.swift
@@ -18,7 +18,11 @@ var rightPaddle = SKSpriteNode()
     
 var score = [Int]()
     
+    
+    
     override func didMove(to view: SKView) {
+        
+        StartGame()
         
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         leftPaddle = self.childNode(withName: "leftPaddle") as! SKSpriteNode
@@ -32,11 +36,37 @@ var score = [Int]()
         self.physicsBody = border
     }
     
+    func StartGame() {
+        score = [0,0]
+    }
+    
+    func addScore(playerWhoScored: SKSpriteNode) {
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        
+        if playerWhoScored == leftPaddle {
+            score[0] += 1
+            
+             ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+        }
+        else if playerWhoScored == rightPaddle {
+            score[1] += 1
+            
+             ball.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
+        }
+        print(score)
+    }
+    
     override func update(_ currentTime: TimeInterval) {
       
         rightPaddle.run(SKAction.moveTo(y: ball.position.y, duration: 0.3))
         
-        
+        if ball.position.x <= leftPaddle.position.x - 30 {
+            addScore(playerWhoScored: rightPaddle)
+        }
+        else if ball.position.x >= rightPaddle.position.x + 30 {
+            addScore(playerWhoScored: leftPaddle)
+        }
      
     }
     
